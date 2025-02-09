c473263bf8dafe5c348be8ea01a6f8eca7c84f57
Islom Karimov <islom@kali.kali>
2025-02-10 03:10:45 +0500

    4-kun

---
+++ core/serializers.py
@@ -0,0 +1,79 @@
+from rest_framework import serializers
+from .models import Student,Category,Cycle,Course,CoursesPerCycle,Teacher,TeachersPerCourse,Enrollment,Class,Attendance,Test ,TestScore
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'  # Barcha maydonlarni qo'shish
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'  # Barcha maydonlarni qo'shish
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'  # Barcha maydonlarni qo'shish
+
+
+class CycleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cycle
+        fields = '__all__'  # Barcha maydonlarni qo'shish
+
+
+class CoursesPerCycleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CoursesPerCycle
+        fields = '__all__'  # Barcha maydonlarni qo'shish
+
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = '__all__'  # Barcha maydonlarni qo'shish
+
+
+class TeachersPerCourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TeachersPerCourse
+        fields = '__all__'  # Barcha maydonlarni qo'shish
+
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enrollment
+        fields = '__all__'  # Barcha maydonlarni qo'shish
+
+
+
+class ClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Class
+        fields = '__all__'  # Barcha maydonlarni qo'shish
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attendance
+        fields='__all__'
+
+class TestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Test
+        fields='__all__'
+
+class TestScoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TestScore
+        fields='__all__'
+
+
+
+
+
+
+
+
+
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Student,Category,Cycle,Course,CoursesPerCycle,Teacher,TeachersPerCourse,Enrollment,Class,Attendance,Test ,TestScore
 
-# Create your views here.
+from .serializers import StudentSerializer,CategorySerializer,CourseSerializer,CycleSerializer,
+CoursesPerCycleSerializer,TeacherSerializer,TeachersPerCourseSerializer,EnrollmentSerializer,ClassSerializer,AttendanceSerializer,TestSerializer,TestScoreSerializer
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.object.all()
+    serializer_class = CategorySerializer
+
+class CycleViewSet(viewsets.ModelViewSet):
+    queryset = Cycle.object.all()
+    serializer_class = CycleSerializer
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.object.all()
+    serializer_class = CourseSerializer
+
+class CoursesPerCycleViewSet(viewsets.ModelViewSet):
+    queryset = CoursesPerCycle.object.all()
+    serializer_class = CoursesPerCycleSerializer
+
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.object.all()
+    serializer_class = TeacherSerializer
+
+class TeachersPerCourseViewSet(viewsets.ModelViewSet):
+    queryset = TeachersPerCourse.object.all()
+    serializer_class = TeachersPerCourseSerializer
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.object.all()
+    serializer_class = EnrollmentSerializer
+
+class ClassViewSet(viewsets.ModelViewSet):
+    queryset = Class.object.all()
+    serializer_class = ClassSerializer
+
+class AttendanceViewSet(viewsets.ModelViewSet):
+    queryset = Attendance.object.all()
+    serializer_class = AttendanceSerializer
+
+class TestViewSet(viewsets.ModelViewSet):
+    queryset = Test.object.all()
+    serializer_class = TestSerializer
+
+classTestScoreViewSet(viewsets.ModelViewSet):
+    queryset = TestScore.object.all()
+    serializer_class = TestScoreSerializer
