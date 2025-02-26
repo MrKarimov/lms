eed8a80ff50af91934f44c1d1d19b21729be74a5
Syrio Halfrunt <syriohal@an-g2.tash.21-school.uz>
2025-02-26 14:51:43 +0500

    alowed host

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hug(3x7j(#hd86c126bg65$^hs(918@hao!qzbn4801#&pgtn('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lms-45te.onrender.com', '127.0.0.1']
 
 
 # Application definition
